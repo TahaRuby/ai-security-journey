b509fade86994a33cbed0c1585572db610444970
TahaRuby <tahayaghot@gmail.com>
2026-09-08 17:48:48 +0330

    complete  bmi_calculations for 30_project_R

---
+++ python/30_project_R/Bmi_calculations.py
@@ -0,0 +1,42 @@
+#imports and global variables
+
+
+# gtting  user inputs as height and weight
+
+def get_user_inputs():
+    weight = float(input("enter your weight (kg): "))
+    height = float(input("enter your height (m): "))
+    return weight,height
+
+
+#calculate bmt
+
+def calculate_bmi(weight,height):
+    return weight // (height**2)
+    
+
+#get the bmi result
+
+def get_bmi_result(bmi):
+    print("bmi: {bmi}\n result: \n")
+    if bmi < 18.5:
+        print("Under Weight")
+    elif 18.5 <= bmi < 25 :
+        print("Normal")
+    elif 25 <= bmi < 30:
+        print("Over Weight")
+    elif 30 <= bmi < 35:
+        print("Obese")
+    else :
+        print("Extremely Obese")
+
+
+#create main function to run
+
+def main():
+    weight,height = get_user_inputs()
+    bmi = calculate_bmi(weight,height)
+    get_bmi_result(bmi)
+
+if __name__ == "__main__":
+    main()
